16fee31e32b9ad9b8d20c93303d6d66d49982765
Daniel Saborido Torres <eldqneil@gmail.com>
2025-09-30 15:11:49 +0200

    feat: add register and login views, buttons, htmls

--- race50/views.py
+++ race50/views.py
@@ -1,6 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.db import IntegrityError
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+User = get_user_model()
 
 # Create your views here.
 def index(request):
     return render(request, "race50/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "race50/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "race50/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "race50/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "race50/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "race50/register.html")
